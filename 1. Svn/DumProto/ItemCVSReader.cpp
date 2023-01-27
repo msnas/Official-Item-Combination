b0265d79c9f512158e2b6641500510331084f43b
msnas <msnasdevelopment@gmail.com>
2023-01-27 20:18:14 +0000

    Added itens + dump proto code

---
+++ 1. Svn/DumProto/ItemCVSReader.cpp
@@ -0,0 +1,40 @@
+// Search for:
+		"ITEM_BELT",
+
+// Add after:
+#ifdef ENABLE_MOVE_COSTUME_ATTR
+		"ITEM_MEDIUM",
+#endif
+
+// Search for:
+	static string arSub31[] = { "EXTRACT_DRAGON_SOUL", "EXTRACT_DRAGON_HEART" };
+
+// Add after:
+#ifdef ENABLE_MOVE_COSTUME_ATTR
+	static string arSub36[] = {
+		"MEDIUM_MOVE_COSTUME_ACCE_ATTR",
+		"MEDIUM_MOVE_COSTUME_ATTR"
+	};
+#endif
+
+// Search for:
+		arSub31,
+		0,
+		0,
+		0,
+
+// Add after:
+#ifdef ENABLE_MOVE_COSTUME_ATTR
+		arSub36,
+#endif
+
+// Search for:
+		sizeof(arSub31) / sizeof(arSub31[0]),
+		0,
+		0,
+		0,
+
+// Search for:
+#ifdef ENABLE_MOVE_COSTUME_ATTR
+		sizeof(arSub36) / sizeof(arSub36[0]),
+#endif
